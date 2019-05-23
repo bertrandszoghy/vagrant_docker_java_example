adf8b0ef20d847764d33210d1fa0739079f60861
Bertrand Szoghy <bertrandszoghy@gmail.com>
2019-05-23 19:11:57 -0400

    Docker image changes now persist

--- docker.sh
+++ docker.sh
@@ -2,5 +2,7 @@
 
 # will be run in /home/vagrant/dockerjava
 
-# ~/bo defined in Java src application.properties
-docker run -v h20-data:/bo -p 8080:8080 -t jingju
+# ~/bo defined in Java src application.properties resides in the container under /root
+# To open a prompt in the jingju container:
+# sudo docker exec -t -i `sudo docker container ls | grep jingju | awk '{print $1}'` sh
+docker run -v h20-data:/root -p 8080:8080 -t jingju
